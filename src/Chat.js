42501f5b1198b740d88a2bc88b3daf86825f7345
Duc Nguyen <duogduc98@gmail.com>
2018-06-12 14:26:50 -0400

    add ChatHeader

--- src/Chat.js
+++ src/Chat.js
@@ -1,10 +1,12 @@
 import React,{component} from 'react'
 
+import ChatHeader from './ChatHeader'
+
 class Chat extends React.Component{
     render(){
         return(
             <div className = "Chat">
-                Chat
+                <ChatHeader />
             </div>
         )
     }
--- src/Main.js
+++ src/Main.js
@@ -2,15 +2,22 @@ import React,{ component } from 'react'
 
 import Sidebar from './Sidebar'
 import Chat from './Chat'
+
 class Main extends React.Component {
     render(){
         return(
-            <div className = "Main">
+            <div className = "Main" style = {styles}>
                 <Sidebar />
                 <Chat />
             </div>
         )
     }
+}
+
+const styles = {
+    display : 'flex',
+    alignItems: 'stretch',
+    height: '100vh',
 }
 
 export default Main
