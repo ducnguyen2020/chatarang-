893f5d22dd0be4219c3aaa1281780abad1a80464
Duc Nguyen <duogduc98@gmail.com>
2018-06-12 14:02:35 -0400

    add Main

---
+++ src/Main.js
@@ -0,0 +1,13 @@
+import React,{ component } from 'react'
+
+class Main extends React.Component {
+    render(){
+        return(
+            <div className = "Main">
+                Main
+            </div>
+        )
+    }
+}
+
+export default Main
