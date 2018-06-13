6cc65f5b378c2ccdf6db0d2b3847b4d25d7f661b
Duc Nguyen <duogduc98@gmail.com>
2018-06-13 15:33:45 -0400

    add login logout

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,15 +1,15 @@
 import React from 'react'
 
-import Avatar from './Avatar'
-import UserInfo from './UserInfo';
-import RoomList from './RoomList';
-const Sidebar = (props) => {
+import UserInfo from './UserInfo'
+import RoomList from './RoomList'
+
+const Sidebar = ({ user, signOut }) => {
   return (
     <aside
       className="Sidebar"
       style={styles.sidebar}
     >
-      <UserInfo user ={props.user}/>
+      <UserInfo user={user} signOut={signOut} />
       <h1
         style={{
           ...styles.children,
@@ -42,15 +42,6 @@ const styles = {
     fontSize: '1.2rem',
     marginTop: 0,
   },
-
-  Avatar: {
-    height: "40px",
-    width: "40px",
-    fontSize: "1rem",
-    borderRadius: "20px",
-  }
-
-
 }
 
 export default Sidebar
